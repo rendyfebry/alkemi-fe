e4e4dc22b4695f7874f48af30564c1fe5bc99336
Rendy Febry <rendyfebry@gmail.com>
2018-07-26 20:51:16 +0700

    Make pages area private

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,10 @@
 export default function({ store, redirect, route }) {
   const userIsLoggedIn = !!store.state.auth.user
 
-  const urlRequiresAuth = /^\/admin(\/|$)/.test(route.fullPath)
+  const adminPaths = /^\/admin(\/|$)/.test(route.fullPath)
+  const pagesPaths = /^\/pages(\/|$)/.test(route.fullPath)
+
+  const urlRequiresAuth = adminPaths || pagesPaths
   const urlRequiresNonAuth = /^\/login(\/|$)/.test(route.fullPath)
 
   if (!userIsLoggedIn && urlRequiresAuth) {
